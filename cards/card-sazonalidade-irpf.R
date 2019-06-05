275a1216cd2e41b38978df707ee4da7b1ad7a942
tiagombp <tiagombp@gmail.com>
2019-06-05 17:40:29 -0300

    Update card-sazonalidade-irpf.R

--- cards/card-sazonalidade-irpf.R
+++ cards/card-sazonalidade-irpf.R
@@ -1,17 +1,18 @@
 library(tidyverse)
 library(viridis)
 library(extrafont)
+library(readxl)
 
 loadfonts()
 
 tema <- function(){
   theme_minimal() +
     theme(
-      text = element_text(family = "Open Sans", colour = "grey20"),
+      text = element_text(family = "Montserrat", colour = "grey20"),
       title = element_text(face = "bold", size = 10, color = "#1E4C7A"), 
-      plot.subtitle = element_text(family = "Open Sans Condensed", 
+      plot.subtitle = element_text(family = "Montserrat Thin", 
                                    color = "grey20", face = "plain", size = 10),
-      axis.text = element_text(family = "Open Sans", colour = "grey20", size = 8),
+      axis.text = element_text(family = "Montserrat", colour = "grey20", size = 8),
       plot.caption = element_text(face = "italic"),
       panel.grid.major = element_blank(), 
       panel.grid.minor = element_blank(),
@@ -23,3 +24,25 @@ tema <- function(){
       legend.position = 'none')
 }
 
+meses <- c("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez")
+MESES <- str_to_upper(meses)
+
+nomes_meses <- meses
+names(nomes_meses) <- 1:12
+
+dados_originais <- read.csv2("rec_irpf_2019_04.csv")
+
+dados <- dados_originais %>%
+  select(-2) %>%
+  mutate(Data = lubridate::dmy(Data),
+         ano = lubridate::year(Data),
+         mes = lubridate::month(Data),
+         nome_mes = factor(meses[mes], levels = meses))
+
+ggplot(dados, aes(y = valor, x = nome_mes, color = ano, group = ano)) +
+  geom_line() +
+  coord_polar() +
+  scale_color_viridis(option = "magma", direction = -1) + 
+  tema()
+
+scale_x_discrete(labels = nomes_meses) +
