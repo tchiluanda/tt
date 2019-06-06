2cbb53c62dbddda0e40f462141239f2f2fc73545
tiagombp <tiagombp@gmail.com>
2019-06-06 13:05:13 -0300

    atualização

--- cards/card-sazonalidade-irpf.R
+++ cards/card-sazonalidade-irpf.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(viridis)
 library(extrafont)
 library(readxl)
+library(gganimate)
+library(transformr)
 
 loadfonts()
 
@@ -39,10 +41,59 @@ dados <- dados_originais %>%
          mes = lubridate::month(Data),
          nome_mes = factor(meses[mes], levels = meses))
 
-ggplot(dados, aes(y = valor, x = nome_mes, color = ano, group = ano)) +
-  geom_line() +
-  coord_polar() +
-  scale_color_viridis(option = "magma", direction = -1) + 
-  tema()
+graf_area <- ggplot(dados, aes(y = valor, x = nome_mes, group = ano, fill =  ano)) +
+  geom_area(color = NA, stat = "identity", position = "identity", alpha = 0.2) +
+  coord_polar(start = -pi/12) +
+  #scale_color_viridis(option = "magma", direction = 1) + 
+  scale_fill_viridis(option = "magma", 
+                     direction = -1, 
+                     breaks = c(max(dados$ano), min(dados$ano))) +
+  scale_y_continuous(labels = function(v){format(v/1e6, big.mark = ".", decimal.mark = ",")})+
+  labs(x = NULL, y = NULL, fill = NULL) +
+  tema() + theme(panel.grid.major.x = element_line(size = 0.05,  color = "#DDDDDD",
+                                                   linetype = "dotted"),
+                 legend.text = element_text(size = 8),
+                 legend.position = "right")
 
-scale_x_discrete(labels = nomes_meses) +
+graf <- ggplot(dados, aes(y = valor, x = nome_mes, group = ano)) +
+  geom_polygon(aes(color = ano), fill = NA) + # geom_polygon fecha a figura do ano
+  #geom_line(aes(color = ano)) +
+  coord_polar(start = -pi/12) +
+  scale_color_viridis(option = "magma", 
+                     direction = -1, 
+                     breaks = c(max(dados$ano), min(dados$ano)),
+                     guide = guide_colourbar(ticks = FALSE)) +
+  # scale_fill_viridis(option = "magma", 
+  #                    direction = 1, 
+  #                    breaks = c(max(dados$ano), min(dados$ano))) +
+  scale_y_continuous(labels = function(v){format(v/1e6, big.mark = ".", decimal.mark = ",")})+
+  labs(x = NULL, y = "R$ milhões", fill = NULL, color = NULL) +
+  tema() + theme(panel.grid.major.x = element_line(size = 0.05,  color = "#111111",
+                                                   linetype = "dotted"),
+                 legend.text = element_text(size = 8),
+                 legend.position = "right",
+                 axis.title.y = element_text(hjust = 0.6)) #angle = 0
+
+graf_anim <- graf + theme(legend.position = "none") + labs(title = '{closest_state}') +
+  transition_states(ano,
+                    transition_length = 2,
+                    state_length = 1) +
+  #ease_aes('quadratic-in-out') +
+  shadow_mark(alpha = .5, color = "grey40")
+
+
+graf_anim %>% animate(type = "cairo", nframes = 150)
+
+anim_save("despesas_new.gif", animation = last_animation())
+
+ggsave("graf.png", dpi = 400)
+
+png(filename="graf.png", 
+    type="cairo",
+    units="in", 
+    width=9.36, 
+    height=6, 
+    pointsize=12, 
+    res=400)
+graf
+dev.off()
