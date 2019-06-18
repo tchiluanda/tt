10c2f59d92a2eccfa41a8014f0b981843a0afbf3
tiagombp <tiagombp@gmail.com>
2019-06-18 16:36:58 -0300

    Update gera_base.R

--- gera_base.R
+++ gera_base.R
@@ -1,11 +1,13 @@
 ### Script para gerar a base que é disponibilizada no TT/ckan
 
 library(readxl)
+library(writexl)
 library(stringr)
 library(lubridate)
 library(tidyverse)
 
-# montando uma base anterior ----------------------------------------------
+
+# listando os arquivos ----------------------------------------------------
 
 arquivos_exceto_RGPS <- list.files(path = "./dados", pattern = "*ex RGPS Pgtos Totais Esf-Acao-Mod.xlsx", full.names = T)
 
@@ -18,22 +20,26 @@ exercicio_atual <- as.character(year(Sys.Date()))
 ### achando a posição do exercício atual na lista de arquivos exceto RGPS
 vetor_pos_exRGPS <- str_detect(arquivos_exceto_RGPS, exercicio_atual)
 
-arq_atual_exceto_RGPS <- arquivos_exceto_RGPS[ vetor_pos_exRGPS]
-arquivos_exceto_RGPS  <- arquivos_exceto_RGPS[!vetor_pos_exRGPS]
+arq_atual_exceto_RGPS    <- arquivos_exceto_RGPS[ vetor_pos_exRGPS]
+arq_fechado_exceto_RGPS  <- arquivos_exceto_RGPS[!vetor_pos_exRGPS]
 
 ### achando a posição do exercício atual na lista de arquivos apenas RGPS
 vetor_pos_soRGPS <- str_detect(arquivos_apenas_RGPS, exercicio_atual)
 
-arq_atual_exceto_RGPS <- arquivos_exceto_RGPS[ vetor_pos_soRGPS]
-arquivos_exceto_RGPS  <- arquivos_exceto_RGPS[!vetor_pos_soRGPS]
+arq_atual_apenas_RGPS    <- arquivos_apenas_RGPS[ vetor_pos_soRGPS]
+arq_fechado_apenas_RGPS  <- arquivos_apenas_RGPS[!vetor_pos_soRGPS]
 
 
 # gerando as bases fechadas -----------------------------------------------
 
-base_fechada_ex_RGPS <- purrr::map(.x = arquivos_exceto_RGPS, .f = read_excel, skip = 9) %>%
+#=========================================================================#
+# só precisa fazer uma vez                                                #
+#=========================================================================#
+
+base_fechada_ex_RGPS <- purrr::map(.x = arq_fechado_exceto_RGPS, .f = read_excel, skip = 9) %>%
   bind_rows()
 
-base_fechada_so_RGPS <- purrr::map(.x = arquivos_apenas_RGPS, .f = read_excel, skip = 9) %>%
+base_fechada_so_RGPS <- purrr::map(.x = arq_fechado_apenas_RGPS, .f = read_excel, skip = 9) %>%
   bind_rows()
 
 # cria classificador para indicar se a despesa é ou não do RGPS
@@ -49,59 +55,88 @@ base_fechada_ex_RGPS <- NULL
 
 save(base_fechada, file = "base_fechada.RData")
 
-##### com sorte, não precisa mais fazer esse pedaço de cima.
+#=========================================================================#
+# com sorte, não precisa mais fazer esse pedaço de cima.                  #
+#=========================================================================#
 
-dados_desp_RGPS <- NULL
 
-# arrumando um pouco
-colnames(dados_desp) <- c("Esfera_cod",
-                          "Esfera_nome",
-                          "Funcao_cod",
-                          "Funcao_nome",
-                          "Subfuncao_cod",
-                          "Subfuncao_nome",
-                          "Programa_cod",
-                          "Programa_nome",
-                          "Acao_cod",
-                          "Acao_nome",
-                          "GND_cod",
-                          "GND_nome",
-                          "Modalidade_id",
-                          "Modalidade_cod",
-                          "Modalidade_nome",
-                          "Elemento_id",
-                          "Elemento_cod",
-                          "Elemento_nome",
-                          "Fonte_id",
-                          "Fonte_cod",
-                          "Fonte_nome",
-                          "ResultadoPrim_cod",
-                          "ResultadoPrim_nome",
-                          "ExcecaoProgFin_cod",
-                          "Ano",
-                          "Mes",
-                          "Periodo",
-                          "ItemInfo_id",
-                          "ItemInfo_nome",
-                          "ItemInfo_cod",
-                          "SaldoAnterior",
-                          "SaldoAtual",
-                          "DespesaRGPS")
+# incorporando a base do ano ----------------------------------------------
 
-dados <- dados_desp %>%
+load("base_fechada.RData")
+
+base_atual_ex_RGPS <- read_excel(arq_atual_exceto_RGPS, skip = 9)
+base_atual_so_RGPS <- read_excel(arq_atual_apenas_RGPS, skip = 9)
+
+# criando as variáveis RGPS
+base_atual_ex_RGPS$RGPS <- FALSE
+base_atual_so_RGPS$RGPS  <- TRUE
+
+# junta as duas tabelas atuais
+base_atual <- bind_rows(base_atual_ex_RGPS, 
+                        base_atual_so_RGPS)
+
+# liberando um pouco de recursos
+base_atual_so_RGPS <- NULL
+base_atual_ex_RGPS <- NULL
+
+
+# fechando a base completa e tratando os dados ----------------------------
+
+base_completa <- bind_rows(base_atual,
+                           base_fechada)
+
+# liberando recursos
+base_fechada <- NULL
+base_atual   <- NULL
+
+
+# limpeza e arrumação dos dados -------------------------------------------
+
+colnames(base_completa) <- c("Esfera_cod",
+                             "Esfera_nome",
+                             "Funcao_cod",
+                             "Funcao_nome",
+                             "Subfuncao_cod",
+                             "Subfuncao_nome",
+                             "Programa_cod",
+                             "Programa_nome",
+                             "Acao_cod",
+                             "Acao_nome",
+                             "GND_cod",
+                             "GND_nome",
+                             "Modalidade_cod",
+                             "Modalidade_nome",
+                             "ResultadoPrim_cod",
+                             "ResultadoPrim_nome",
+                             "ExcecaoProgFin_cod",
+                             "Periodo",
+                             "Ano",
+                             "Mes",                             
+                             "Valor",
+                             "DespesaRGPS")
+
+# verificação -- tem uma diferença em 2008, no exceto RGPS, no próprio TG.
+
+base_completa %>% 
+  #filter(Ano == "2019", as.numeric(Mes)<=4) %>% 
+  group_by(Ano) %>% 
+  summarise(total = as.character(sum(Valor))) 
+  #%>% write.csv2(file = "verifica.csv")
+
+# arrumação
+
+dados <- base_completa %>%
   filter(!(Mes %in% c(13, 14))) %>%
   mutate(Periodo = as.Date(paste0(Ano, "-",
                                   str_pad(Mes, width = 2, pad = "0"), "-",
-                                  "01")),
-         SaldoAnterior = as.numeric(as.character(SaldoAnterior)),
-         SaldoAtual = as.numeric(as.character(SaldoAtual))) %>%
-  replace_na(list(SaldoAnterior = 0, SaldoAtual = 0)) %>%
-  mutate(Valor = round(SaldoAtual - SaldoAnterior, 2)) %>%
+                                  "01"))) %>%
   filter(!(Funcao_cod %in% c("-7","-9")) & 
-           !(GND_cod %in% c("-7","-9")) &
-           !(Modalidade_cod %in% c("-7","-9"))) %>%
-  select(-SaldoAtual, -SaldoAnterior, -ends_with("_id"))
+         !(GND_cod %in% c("-7","-9")) &
+         !(Modalidade_cod %in% c("-7","-9")))
 
-dados_desp %>% 
-  write.csv2("./dados/despUniao.csv", row.names = FALSE)
 
+dados %>% 
+  write.csv2("despesas_uniao.csv", row.names = FALSE)
+
+dados %>%
+  write_xlsx("despesa_uniao.xlsx")
