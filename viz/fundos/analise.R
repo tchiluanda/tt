bc5d6325ec40455564d5a915516432f09f318b24
tiagombp <tiagombp@gmail.com>
2019-11-08 16:33:26 -0300

    Create analise.R
    deveria ter feito commits antes, agora já está bem ajustado

---
+++ viz/fundos/analise.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+
+
+
+# construindo uma base para a visualizacao --------------------------------
+
+qde_fundos_visados  <- 163
+qde_fundos_com_fin  <- 72
+qde_fundos_sem_fin  <- qde_fundos_visados - qde_fundos_com_fin
+qde_fundos_com_sf   <- 47
+qde_fundos_com_bp   <- 62
+qde_fundos_com_sfbp <- 37
+qde_fundos_so_sf    <- qde_fundos_com_sf - qde_fundos_com_sfbp
+qde_fundos_so_bp    <- qde_fundos_com_bp - qde_fundos_com_sfbp
+
+lista_fundos <- c("Sem Financeiro" = qde_fundos_sem_fin, 
+                  "Apenas SF"      = qde_fundos_so_sf, 
+                  "SF e BP"        = qde_fundos_com_sfbp, 
+                  "Apenas BP"      = qde_fundos_so_bp)
+
+tipos_fundos <- names(lista_fundos)
+
+vlrs_fundos  <- data.frame(
+  "Tipo_fundo" = tipos_fundos,
+  "Valores"    = c(0, 48.7, 175.4, 257.8-173)
+)
+
+cria_df <- function(item_fundos) {
+  df <- data.frame(
+    "Sequencial_tipo" = 1:item_fundos,
+    "Tipo_fundo"      = rep(names(item_fundos), item_fundos) # (1)
+  )
+  return(df)
+}
+
+## Obs: (1) no map, ele não passa os "names" do vetor.
+
+
+# a <- cria_df(lista_fundos[2])
+
+## teste map
+
+  cria_df2 <- function(item_fundos, item_tipos) {
+    df <- data.frame(
+      "Sequencial_tipo" = 1:item_fundos,
+      "Tipo_fundo"      = rep(item_nomes, item_fundos)
+    )
+    return(df)
+  }
+  
+  lista_map <- map2(lista_fundos, tipos_fundos, cria_df2)
+
+## fim teste map
+  
+
+lista <- list()
+
+for (i in 1:4) {
+  lista[[i]] <- cria_df(lista_fundos[i])
+}
+
+base_fundos <- bind_rows(lista)
+
+largura_plot <- 4
+padding_entre_grupos <- 0.5
+
+qde_linhas_por_grupo <- ((lista_fundos-1)%/%largura_plot) + 1
+
+### observar a diferença sutil entre os dois vetores abaixo!
+#lag(cumsum(qde_linhas_por_grupo), 1, default = 0)
+#((lag(cumsum(lista_fundos), 1, default = 0) - 1) %/% largura_plot) + 1
+
+tabela_padding <- data.frame(
+  Tipo_fundo   = tipos_fundos,
+  Padding      = 1:length(tipos_fundos) * padding_entre_grupos,
+  pos_inicial  = lag(cumsum(qde_linhas_por_grupo), 1, default = 0)
+)
+
+
+fundos <- base_fundos %>%
+  left_join(vlrs_fundos) %>%
+  mutate(x = ((Sequencial_tipo - 1) %%  largura_plot) + 1,
+         y = ((Sequencial_tipo - 1) %/% largura_plot) + 1) %>%
+  left_join(tabela_padding) %>%
+  mutate(y = y + pos_inicial + Padding)
+
+ggplot(fundos, aes(x = x, y = y, fill = Tipo_fundo)) + 
+  geom_tile(color = "white", width = 0.75, height = 0.75) + 
+  scale_x_continuous(limits = c(NA, 20))
+
+
