8b967b111c6617a13a2299f3c548673724c2cf2f
tiagombp <tiagombp@gmail.com>
2019-11-12 17:42:52 -0300

    Update analise.R

--- datasets/alugueis/analise.R
+++ datasets/alugueis/analise.R
@@ -3,7 +3,10 @@ library(tidyverse)
 dados_brutos <- read.csv2("alugueis2011_2019_real.csv")
 
 dados <- dados_brutos %>%
-  mutate(saldo = as.numeric(as.character(SALDORITEMINFORMAODIALANAMENT))) %>%
+  mutate(saldo = as.numeric(as.character(SALDORITEMINFORMAODIALANAMENT)),
+         tipo_doc = str_sub(ID_DOCUMENTO_LANC, 16, 17)) %>%
+  count(tipo_doc)
+  
   group_by(ID_ANO_LANC, CO_NATUREZA_DESPESA_DETA) %>%
   summarise(saldo = sum(saldo)) %>%
   spread(key = ID_ANO_LANC, value = saldo)
